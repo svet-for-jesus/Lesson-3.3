452edd6814c83f91f5df38094dd3b39bc8a7fafa
svet-for-jesus <melnikov.svetozar@gmail.com>
2024-10-01 13:29:26 +0300

    add new code

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import pygame
-pygame.init()
+from pygame.examples.go_over_there import screen
 
+pygame.init()
+screen_width = 800
+screen_height = 600
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption("Игра тир")
+icon = pygame.image.load("")
 running = True
 while running:
     pass
