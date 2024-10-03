e3169cc7b8c59a61e0aa503133e85621ae0024a6
svet-for-jesus <melnikov.svetozar@gmail.com>
2024-10-03 16:53:13 +0300

    add new code

--- main.py
+++ main.py
@@ -29,6 +29,7 @@ while running:
         if event.type == pygame.MOUSEBUTTONDOWN:
             mouse_x, mouse_y =pygame.mouse.get_pos()
             if target_x < mouse_x < target_x + target_width and target_y < mouse_y < target_y + target_height:
+                screen.blit(target2_img, (target_x + target_width, target_y+ target_height))
                 target_x = random.randint(0, SCREEN_WIDTH - target_width)
                 target_y = random.randint(0, SCREEN_HEIGHT - target_height)
    screen.blit(target_img, (target_x, target_y))
